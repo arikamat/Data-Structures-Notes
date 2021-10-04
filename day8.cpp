b2506a18a42318061d9f5303b9570ca22f199dcd
Ari Kamat <arijkamat@gmail.com>
2021-10-04 08:50:38 -0400

    Day8 and Day 9 notes added

---
+++ day8.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using std::cout;
+
+int main(){
+
+}
+/*
+ Queues
+ ##########
+ Add to the back & pop from the front
+
+ Input-Restrcited Deque
+
+ Greatest Common Divisor - Euclid's Algorithm:
+ int gcd(int a, int b){
+    while(b){
+        int r = a%b;
+        a=b;
+        b=r;
+    }
+    //most inefficient especially for fibonacci number like gcd(55,34)
+    //note: Fibonacci number F_0 = 9, F_1 = 1, F_n = F_n-1+F_n-2, n>1; F_n ~ (1/sqrt5)phi^n where phi = (1+sqrt5)/2
+    Efficiency - O(lg(min(a,b)))
+ }
+
+ Extended GCD (Bezout)
+ Theorem: If d=gcd(a,b), then exists x,y integers such that d=ax+by
+
+
+ Theorem: If d=gcd(a,b), then exists a x,y integers
+ Tail Recorsion - Recursion where the recursive call is the last executable statement; Tail recursion can always be implemented as a lopp
+ leo levim
+
+ int gcd(int a, int b){
+    while(b){
+
+    }
+*/
---
+++ day9.cpp
@@ -0,0 +1,4 @@
+//
+// Created by arikam22 on 10/4/2021.
+//
+
